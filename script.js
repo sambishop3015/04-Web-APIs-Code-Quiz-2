de55d4a31c7166c28a286e2929f67bc1c81b4d7e
Sam Bishop <sambishop3015@hotmail.com>
2020-10-04 23:04:23 -0400

    Start Btn Func, Hide, Random Math, Question Fill

---
+++ script.js
@@ -0,0 +1,44 @@
+//Make sure the html is loaded before running script
+$(document).ready(function() {
+
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+
+let shuffledQuestions, currentQuestionIndex;
+
+$(startButton).on('click', startGame);
+
+function startGame() {
+    console.log('Started');
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
+function selectAnswer() {
+
+}
+
+const questions = [
+    {
+        question: 'What is 2 + 2?',
+        answers: [
+            {text: '4', correct: true},
+            {text: '22', correct: false}
+        ]
+    }
+]
+
+})
